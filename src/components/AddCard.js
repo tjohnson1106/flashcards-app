d5888d0c6ae8042833ef784243a2f37c28e185d5
Thomas Johnson <tjohnson1106@gmail.com>
2018-05-24 16:45:40 -0400

    added styles and package values to add card component

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -16,7 +16,55 @@ import { addCard } from "../actions";
 
 class AddCard extends Component {
   render() {
-    return <View>{}</View>;
+    return (
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={styles.container}
+      >
+        <View style={styles.container}>
+          <Text style={styles.title}>{}</Text>
+          <TextInput
+            style={styles.inputText}
+            onChangeText={question =>
+              this.setState({
+                question
+              })
+            }
+            value={this.state.question}
+          >
+            {}
+          </TextInput>
+          <Text style={styles.title}>{}</Text>
+          <TextInput
+            style={styles.inputText}
+            onChangeText={answer =>
+              this.setState({
+                answer
+              })
+            }
+            value={this.state.answer}
+          >
+            {}
+          </TextInput>
+          <Text style={styles.title}>{}</Text>
+          <TextInput
+            style={styles.inputText}
+            onChangeText={correctAnswer =>
+              this.setState({
+                correctAnswer
+              })
+            }
+            value={this.state.correctAnswer}
+          >
+            {}
+          </TextInput>
+
+          <TouchableOpacity>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
   }
 }
 
@@ -25,6 +73,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  submitButtonText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  title: {
+    fontSize: 30,
+    color: "#333"
+  },
+  submitButton: {
+    borderWidth: 0.5,
+    borderColor: "#d6d7da",
+    padding: 10,
+    backgroundColor: orange,
+    borderRadius: 7,
+    overflow: "hidden"
+  },
+  inputText: {
+    width: 250,
+    height: 40,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "#757575",
+    margin: 20,
+    borderRadius: 7
   }
 });
 
